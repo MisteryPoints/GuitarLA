184a6a9cc3c5eb3044ef6b829cd083362d778b85
MisteryPoints <misterypoint2@gmail.com>
2022-07-27 22:03:20 -0400

    First House Update

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,23 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import styles from '../styles/Header.module.css'
 
 const Header = () => {
   return (
     <header className={styles.header}>
-        <div className='container'>
-            <div>
-
-            </div>
-            <nav>
-                <Link href='/'>Inicio</Link>
-                <Link href='/nosotros'>Nosotros</Link>
-                <Link href='/blog'>Blog</Link>
-                <Link href='/tienda'>Tienda</Link>
-            </nav>
+      <div className='container'>
+        <div className={styles.barra}>
+          <Link href='/' >
+            <Image className={styles.logo} width={400} height={100} src='/img/logo.svg' alt='Imagen Logo'/>
+          </Link>
+          <nav className={styles.navegacion}>
+            <Link href='/'>Inicio</Link>
+            <Link href='/nosotros'>Nosotros</Link>
+            <Link href='/blog'>Blog</Link>
+            <Link href='/tienda'>Tienda</Link>
+          </nav>
         </div>
+      </div>
     </header>
   )
 }
